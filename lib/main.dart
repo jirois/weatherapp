35c23c7d86d26334cc1fc840002dee821b1abc6a
Omas Ajiri <jicintech@gmail.com>
2025-11-14 17:10:58 +0100

    feat: added .env and complete the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'weather_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/config/secret.env");
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ).copyWith(appBarTheme: AppBarTheme()),
+      home: const WeatherScreen(),
+    );
   }
 }
